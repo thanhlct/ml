cba17db15b4300c3ca3ca1b02b9de362c1de0803
Thanh Le <thanhlct@gmail.com>
2017-10-26 12:09:56 +0700

    Finish first experiment with CRF on NER

--- apps/ner/crf_ner.py
+++ apps/ner/crf_ner.py
@@ -1,9 +1,16 @@
-#the source mainly refers to:
+#the source mainly refers from:
 #- https://github.com/TeamHG-Memex/sklearn-crfsuite/blob/master/docs/CoNLL2002.ipynb
 #- https://sklearn-crfsuite.readthedocs.io/en/latest/tutorial.html#let-s-use-conll-2002-data-to-build-a-ner-system 
 
 import sklearn_crfsuite
 from sklearn_crfsuite import metrics
+
+import pickle
+import scipy.stats
+
+from sklearn.metrics import make_scorer
+from sklearn.grid_search import RandomizedSearchCV
+
 from data_reader import read_vlsp_data
 
 import pdb
@@ -58,25 +65,22 @@ def sent2labels(sent):
     return [label for token, postag, chunk_label, label, _ in sent]
 
 
-if __name__ == '__main__':
-    print('Loading corpus...')
-    train, valid, test = read_vlsp_data('../../corpora/ner/vlsp/')
-
-    print('Extracting features...')
-    trainx = [sent2features(sent) for sent in train]
-    trainy = [sent2labels(sent) for sent in train]
-
-    testx = [sent2features(sent) for sent in test]
-    testy = [sent2labels(sent) for sent in test]
-
+def run1(trainx, trainy, validx, validy, testx, testy):
     print('Training model...')
     crf = sklearn_crfsuite.CRF(
         algorithm='lbfgs', 
-        c1=0.1, 
-        c2=0.1, 
+        #c1=0.1, 
+        #c2=0.1,
+        c1=0.045677,
+        c2=0.069943,
         max_iterations=100, 
         all_possible_transitions=True
     )
+    
+    #append train and valid set
+    trainx.extend(validx)
+    trainy.extend(validy)
+
     crf.fit(trainx, trainy)
     
     print('Evaluating model...')
@@ -90,6 +94,85 @@ if __name__ == '__main__':
     predict_y = crf.predict(testx)
     print('Test: ', metrics.flat_f1_score(testy, predict_y,
                       average='weighted', labels=labels))
+
+    #Insepect per-class results
+    print(metrics.flat_classification_report(testy, predict_y))
+
+    #save/load model
+    save_model(crf, 'ner_crf.stand.pkl')
+    load_model('ner_crf.stand.pkl', testx, testy, labels)
+
+def save_model(model, fpath):
+    with open(fpath, 'wb') as f:
+        pickle.dump(model, f)
+
+def load_model(fpath, testx, testy, labels):
+    with open(fpath, 'rb') as f:
+        crf = pickle.load(f)
+
+        predict_y = crf.predict(testx)
+        print('Test when reload: ', metrics.flat_f1_score(testy, predict_y,
+                      average='weighted', labels=labels))
+
+def run2(trainx, trainy, validx, validy, testx, testy):
+    print('Optimizing hyperparameters...')
+    crf = sklearn_crfsuite.CRF(
+        algorithm='lbfgs', 
+        max_iterations=100, 
+        all_possible_transitions=True
+    )
+    
+    params_space = {
+        'c1': scipy.stats.expon(scale=0.5),
+        'c2': scipy.stats.expon(scale=0.05),
+    }
+
+    labels = ['B-PER', 'I-PER', 'B-LOC', 'B-ORG', 'I-LOC', 'B-MISC', 'I-MISC', 'I-ORG', 'B-NP', 'B-MICS', 'I-MICS'] 
+    f1_scorer = make_scorer(metrics.flat_f1_score, average='weighted', labels=labels)
+    
+    rs = RandomizedSearchCV(crf, params_space,
+                        cv=3,
+                        verbose=1,
+                        n_jobs=-1,
+                        n_iter=50,
+                        scoring=f1_scorer)
+
+    trainx.extend(validx)
+    trainy.extend(validy)
+    trainx.extend(testx)
+    trainy.extend(testy)
+
+    rs.fit(trainx, trainy)
+
+    print('best params:', rs.best_params_)
+    print('best CV score:', rs.best_score_)
+    print('model size: {:0.2f}M'.format(rs.best_estimator_.size_ / 1000000))
+
+def main():
+    print('Loading corpus...')
+    train, valid, test = read_vlsp_data('../../corpora/ner/vlsp/')
+
+    print('Extracting features...')
+    trainx = [sent2features(sent) for sent in train]
+    trainy = [sent2labels(sent) for sent in train]
+
+    validx = [sent2features(sent) for sent in valid]
+    validy = [sent2labels(sent) for sent in valid]
+
+    testx = [sent2features(sent) for sent in test]
+    testy = [sent2labels(sent) for sent in test]
+
+    run1(trainx, trainy, validx, validy, testx, testy)#normal
+    #run2(trainx, trainy, validx, validy, testx, testy)#optimize hyperparameters
+   
+
+if __name__ == '__main__':
+    main()
+
 '''=======Results notes:
-- Oct 20: train: 0.994, test: 0.889
+- Oct 20: train: 0.994, test: 0.897725
+- Oct 26: 3-fold optimizing hyperparameters got: 0.9039 with c1=0.045677 and c2=0.069943
+- Oct 26: Run best hyperparameters, got model: 0.8983624 
+
+CNN-biLSTM-CRF, https://arxiv.org/pdf/1705.04044.pdf, got 88.38 with complexity and 80.
 '''
